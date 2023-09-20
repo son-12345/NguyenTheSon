5df4430d3af5cb651d6450485f105b9fdeb7dd78
son-12345 <142959430+son-12345@users.noreply.github.com>
2023-09-19 21:21:50 -0400

    Add files via upload

---
+++ tính chu vi dien tích cua hcn.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+	float width,height;
+	printf("width of rectangle:");
+	scanf("%f",&width);
+	printf("height of rectangle:");
+	scanf("%f",&height);
+	printf("\n Perimeter of the rectangle: %f",(width+height)*2);
+	printf("\n Area of the rectangle: %f", width*height); 
+	
+}
